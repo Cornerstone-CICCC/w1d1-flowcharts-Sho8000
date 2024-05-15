7159f2572927888f21182de9f6b3e59755ea51b6
Sho8000 <yoshikichi8000@gmail.com>
2024-05-16 08:36:55 +0900

    EX5 update

--- EX5.js
+++ EX5.js
@@ -1,3 +1,4 @@
+/* 
 var studentname = "Sho";
 var score = 20;
 
@@ -14,9 +15,28 @@ else{
   console.log(studentname + " => No Grade");
 }
 
+ */
+
 /* we can use array for judgement value
 
 var grade = [80, 60, 40]
  ->grade[0],[1],[2]
 
 */
+
+var studentname = "Sho";
+var score = 21;
+var scoregrade = [80,60,40];
+
+if(score>=scoregrade[0]){
+  console.log(studentname + " => Grade A");
+}
+else if(score>=scoregrade[1]){
+  console.log(studentname + " => Grade B");
+}
+else if(score>=scoregrade[2]){
+  console.log(studentname + " => Grade C");
+}
+else{
+  console.log(studentname + " => No Grade");
+}
